a0cca8ad558ed8da82f223ef8f9ce4344be480ed
martinkribs <martin.kribs.mk@gmail.com>
2023-03-30 14:18:01 +0200

    output

--- return.c
+++ return.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 int a = 0, b = 1;
 void bar(int b)
 {
     a = b;
+    printf("bar, a=%d\n", a);
+    printf("bar, b=%d\n", b);
 }
 void foo(int a, int b)
 {
@@ -9,6 +12,8 @@ void foo(int a, int b)
         int b = a;
         int a = a + b;
     }
+    printf("foo, a=%d\n", a);
+    printf("foo, b=%d\n", b);
     bar(a);
 }
 int main(void)
@@ -18,5 +23,7 @@ int main(void)
         int a = 2;
         foo(a, b);
     }
+    printf("main, a=%d\n", a);
+    printf("main, b=%d\n", b);
     return a;
 }
